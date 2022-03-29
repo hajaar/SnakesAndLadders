00e2a60b80963fc2b5b06df18aec1f0b11ce114f
Kartik Narayanan <1748756+hajaar@users.noreply.github.com>
2022-03-29 08:45:53 +0530

    add UIColor Extension to convert RGB values into UIColor format

--- SnakesAndLadders/Helpers/Colors.swift
+++ SnakesAndLadders/Helpers/Colors.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Colors {
     private static let mattPurple = UIColor(red: 0.55, green: 0.48, blue: 0.90, alpha: 1.00)
@@ -17,4 +18,32 @@ struct Colors {
     static let boardColor = skirretGreen
     static let highlightTileColor = nanohanachaGold
     static let tileColor = (lightBlue, darkBlue)
+}
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
 }
