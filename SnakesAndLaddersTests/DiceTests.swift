6fd1f01210ce1abd18db082c76b031f9e2f17934
Kartik Narayanan <hajaar@icloud.com>
2022-03-31 16:20:01 +0530

    split some unit tests in DiceTests to ensure that only one condition is tested

--- SnakesAndLaddersTests/DiceTests.swift
+++ SnakesAndLaddersTests/DiceTests.swift
@@ -31,27 +31,45 @@ class DiceTests: XCTestCase {
         XCTAssertEqual(Dice.returnNumberOfSides(), 12)
     }
     
-    func testRollSingleDieWithSixSidesReturnsValueBetweenOneAndSix() {
+    func testRollSingleDieWithSixSidesReturnsValueGreaterThanOrEqualToOne() {
         Dice.create(numberOfDice: 1, numberOfSides: 6)
         Dice.roll()
-        XCTAssertLessThanOrEqual(Dice.returnRollSum(), Dice.returnNumberOfSides())
         XCTAssertGreaterThanOrEqual(Dice.returnRollSum(), 1)
     }
     
-    func testRollThreeDiceWithSixSidesReturnsValueBetweenThreeAndEighteen() {
+    func testRollSingleDieWithSixSidesReturnsValueLesserThanOrEqualToSix() {
+        Dice.create(numberOfDice: 1, numberOfSides: 6)
+        Dice.roll()
+        XCTAssertLessThanOrEqual(Dice.returnRollSum(), Dice.returnNumberOfSides())
+    }
+    
+    func testRollThreeDiceWithSixSidesReturnsValueGreaterThanOrEqualToThree() {
         Dice.create(numberOfDice: 3, numberOfSides: 6)
         Dice.roll()
-        XCTAssertLessThanOrEqual(Dice.returnRollSum(), 3 * Dice.returnNumberOfSides())
         XCTAssertGreaterThanOrEqual(Dice.returnRollSum(), 3)
     }
     
-    func testRollThreeDiceWithSixSidesReturnsIndividualValuesBetweenOneAndSix() {
+    func testRollThreeDiceWithSixSidesReturnsValueLesserThanOrEqualToEighteen() {
+        Dice.create(numberOfDice: 3, numberOfSides: 6)
+        Dice.roll()
+        XCTAssertLessThanOrEqual(Dice.returnRollSum(), 3 * Dice.returnNumberOfSides())
+    }
+    
+    func testRollThreeDiceWithSixSidesReturnsIndividualValuesGreaterThanOrEqualToOne() {
+        Dice.create(numberOfDice: 3, numberOfSides: 6)
+        Dice.roll()
+        let tmpRoll = Dice.returnRollValues()
+        for i in 0...2 {
+            XCTAssertGreaterThanOrEqual(tmpRoll[i], 1)
+        }
+    }
+    
+    func testRollThreeDiceWithSixSidesReturnsIndividualValuesLesserThanOrEqualToSix() {
         Dice.create(numberOfDice: 3, numberOfSides: 6)
         Dice.roll()
         let tmpRoll = Dice.returnRollValues()
         for i in 0...2 {
             XCTAssertLessThanOrEqual(tmpRoll[i], Dice.returnNumberOfSides())
-            XCTAssertGreaterThanOrEqual(tmpRoll[i], 1)
         }
     }
 
